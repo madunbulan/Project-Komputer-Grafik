ac882fcc36d01928114cc0356b63db1bb2f53d9a
madunbulan <85464475+madunbulan@users.noreply.github.com>
2022-10-24 14:32:14 +0700

    Membuat fungsi untuk membuat box (Elvin & Ryan)

---
+++ js/modeling.js
@@ -0,0 +1,12 @@
+function createBox(dx, dy, dz, color, x, y, z, notFlatShading) {
+    var geom = new THREE.BoxGeometry(dx, dy, dz);
+    var mat = new THREE.MeshPhongMaterial({
+        color: color,
+        flatShading: notFlatShading != true
+    });
+    var box = new THREE.Mesh(geom, mat);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    box.position.set(x, y, z);
+    return box;
+}
